6d83416a99f0e74223d5609b82f51f4663ad8129
ariefpss <permanaarief8@gmail.com>
2022-12-20 19:31:08 +0900

    new file:   public/images/card-portfolio.png 	modified:   routes/home.js 	modified:   views/home.ejs 	new file:   views/partials/footer.ejs 	new file:   views/partials/header.ejs 	new file:   views/portfolio.ejs

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'RiefDev' });
+  res.render('home', { title: 'RiefDev', url: req.originalUrl });
+});
+
+router.get('/portfolio', function(req, res) {
+  res.render('portfolio', { title: 'RefDev', url: req.originalUrl });
 });
 
 module.exports = router;
